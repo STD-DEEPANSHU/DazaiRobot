169b86389f9e0ccb94aee55dee582e0dc3af3191
˹ʜᴧᴍᴋᴇꝛ ꭙ˼ <137426901+HamkerX@users.noreply.github.com>
2023-09-12 23:02:23 +0530

    Update userinfo.py

--- DazaiRobot/modules/userinfo.py
+++ DazaiRobot/modules/userinfo.py
@@ -301,10 +301,10 @@ def info(update: Update, context: CallbackContext):
                     [
                         [
                             InlineKeyboardButton(
-                                "ʜᴇᴀʟᴛʜ", url="https://t.me/The_Apexx/456"
+                                "ʜᴇᴀʟᴛʜ", url="https://t.me/StdBotz/6"
                             ),
                             InlineKeyboardButton(
-                                "ᴅɪꜱᴀꜱᴛᴇʀ", url="https://t.me/The_Apexx/455"
+                                "ᴅɪꜱᴀꜱᴛᴇʀ", url="https://t.me/StdBotz/7"
                             ),
                         ],
                     ]
