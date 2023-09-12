badaf9e249a395658254c9ec860b2bb3b6facfa5
˹ʜᴧᴍᴋᴇꝛ ꭙ˼ <137426901+HamkerX@users.noreply.github.com>
2023-09-12 22:15:27 +0530

    Update whisper.py

--- DazaiRobot/modules/whisper.py
+++ DazaiRobot/modules/whisper.py
@@ -109,7 +109,7 @@ async def in_help():
     answers = [
         InlineQueryResultArticle(
             title="💒 Whisper",
-            description=f"@Dazaiprobot [USERNAME | ID] [TEXT]",
+            description=f"@DeepXRobot [USERNAME | ID] [TEXT]",
             input_message_content=InputTextMessageContent(f"**📍Usage:**\n\n@Dazaiprobot (Target Username or ID) (Your Message).\n\n**Example:**\n@Dazaiprobot @username I Wanna Phuck You"),
             thumb_url="https://te.legra.ph/file/3eec679156a393c6a1053.jpg",
             reply_markup=switch_btn
