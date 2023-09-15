426409256623fbe1e14ee994f2e9ce17751576c5
˹ʜᴧᴍᴋᴇꝛ ꭙ˼ <137426901+HamkerX@users.noreply.github.com>
2023-09-15 21:49:47 +0530

    Update draw.py

--- DazaiRobot/modules/draw.py
+++ DazaiRobot/modules/draw.py
@@ -2,7 +2,7 @@ import asyncio
 import time
 from DazaiRobot import pbot
 from pyrogram import Client, filters
-from DazaiRobot import AsyncClient
+from pbot import AsyncClient
 from pyrogram.types import (
     InlineKeyboardMarkup,
     InlineKeyboardButton,
