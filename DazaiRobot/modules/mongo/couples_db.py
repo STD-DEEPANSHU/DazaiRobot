b85196bfcdc475eb3c0fcfc8d51baca6f5a0c176
˹ʜᴧᴍᴋᴇꝛ ꭙ˼ <137426901+HamkerX@users.noreply.github.com>
2023-09-17 21:30:04 +0530

    Update couples_db.py

--- DazaiRobot/modules/mongo/couples_db.py
+++ DazaiRobot/modules/mongo/couples_db.py
@@ -1,28 +1,39 @@
-from DazaiRobot.utils.mongo import db
+#MIT License
+#Copyright (c) 2023, ©NovaNetworks
 
-coupledb = db.couple
+from DazaiRobot.mongo import *
 
-async def _get_lovers(chat_id: int):
-    lovers = await coupledb.find_one({"chat_id": chat_id})
-    if not lovers:
-        return {}
-    return lovers["couple"]
+coupledb = dbname["couples"] 
 
+async def _get_lovers(cid: int):
+    lovers = await coupledb.find_one({"chat_id": cid})
+    if lovers:
+        lovers = lovers["couple"]
+    else:
+        lovers = {}
+    return lovers
 
-async def get_couple(chat_id: int, date: str):
-    lovers = await _get_lovers(chat_id)
+async def _get_image(cid: int):
+    lovers = await coupledb.find_one({"chat_id": cid})
+    if lovers:
+        lovers = lovers["img"]
+    else:
+        lovers = {}
+    return lovers
+
+async def get_couple(cid: int, date: str):
+    lovers = await _get_lovers(cid)
     if date in lovers:
         return lovers[date]
-    return False
+    else:
+        return False
 
-async def del_couple(chat_id : int):
-    lovers = await coupledb.find_one({"chat_id": chat_id})
-    if lovers :
-        return await coupledb.delete_one({"chat_id": chat_id})
 
-async def save_couple(chat_id: int, date: str, couple: dict):
-    lovers = await _get_lovers(chat_id)
+async def save_couple(cid: int, date: str, couple: dict, img: str):
+    lovers = await _get_lovers(cid)
     lovers[date] = couple
     await coupledb.update_one(
-        {"chat_id": chat_id}, {"$set": {"couple": lovers}}, upsert=True
+        {"chat_id": cid},
+        {"$set": {"couple": lovers, "img": img}},
+        upsert=True,
     )
