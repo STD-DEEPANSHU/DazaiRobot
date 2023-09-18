e11a9193ab3f63d66a8e02d8d7843a6a86830f8b
˹ʜᴧᴍᴋᴇꝛ ꭙ˼ <137426901+HamkerX@users.noreply.github.com>
2023-09-18 09:20:43 +0530

    Update couples.py

--- DazaiRobot/modules/couples.py
+++ DazaiRobot/modules/couples.py
@@ -90,8 +90,8 @@ async def ctest(_, message):
 
          draw = ImageDraw.Draw(img)
 
-         img.paste(img1, (60, 220), img1)
-         img.paste(img2, (821, 230), img2)
+         img.paste(img1, (100, 220), img1)
+         img.paste(img2, (800, 230), img2)
 
          img.save(f'test_{cid}.png')
     
