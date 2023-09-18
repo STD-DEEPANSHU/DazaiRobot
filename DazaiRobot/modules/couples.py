7e1fd4e8ce662ea5c50e293b8e55045ab8fdbeda
˹ʜᴧᴍᴋᴇꝛ ꭙ˼ <137426901+HamkerX@users.noreply.github.com>
2023-09-18 07:58:33 +0530

    Update couples.py

--- DazaiRobot/modules/couples.py
+++ DazaiRobot/modules/couples.py
@@ -73,8 +73,8 @@ async def ctest(_, message):
 
          img = Image.open("DazaiRobot/resources/cp.png")
 
-         img1 = img1.resize((320,320))
-         img2 = img2.resize((320,320))
+         img1 = img1.resize((340,340))
+         img2 = img2.resize((340,340))
 
          mask = Image.new('L', img1.size, 0)
          draw = ImageDraw.Draw(mask) 
@@ -90,8 +90,8 @@ async def ctest(_, message):
 
          draw = ImageDraw.Draw(img)
 
-         img.paste(img1, (90, 200), img1)
-         img.paste(img2, (871, 200), img2)
+         img.paste(img1, (90, 250), img1)
+         img.paste(img2, (871, 250), img2)
 
          img.save(f'test_{cid}.png')
     
